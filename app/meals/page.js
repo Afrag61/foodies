3690df350a667bcb796b02dda9a3558d1c696970
Ahmed Frag <a.frag166@gmail.com>
2025-03-01 01:54:31 +0200

    Outputting Meals Data & Images With Unknown

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,6 +1,27 @@
+import Link from "next/link";
+import styles from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
 const MealsPage = () => {
-    return <h1 style={{ textAlign: "center", color: "white" }}>Meals</h1>;
-  };
-  
-  export default MealsPage;
-  
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. it is easy and fun!
+        </p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={[]}/>
+      </main>
+    </>
+  );
+};
+
+export default MealsPage;
